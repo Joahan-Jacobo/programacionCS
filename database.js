24511e541dea25c0b4703ab4dda833cd2f3c40b3
Joahan-Jacobo <101759341+Joahan-Jacobo@users.noreply.github.com>
2022-03-24 18:32:54 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-	host: "localhost",
-	user: "root",
-	password: "",
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "b96de9beb2242b",
+	password: "af038cd3",
 	port: 3306,
-	database: "adoption_system",
+	database: "heroku_8786d50ec3783fe",
 	secret_key: "azdcgbjmasdfghjklñ"
 };
 exports.config = config;
